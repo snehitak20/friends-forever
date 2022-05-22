77e6d330fdd25de2b413fbbdeb873b8f1b446798
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-22 00:12:01 -0700

    delete config folder

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/friends-forever', {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
